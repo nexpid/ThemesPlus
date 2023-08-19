88242e336df84aa6774f8928ba70edf1d1071348
Gabe616 <poggerschampersqwq@gmail.com>
2023-08-19 20:47:00 +0200

    feat(workflows): add iconpack trees writer workflow

---
+++ .github/scripts/iconpacks_tree_writer/index.mjs
@@ -0,0 +1,54 @@
+import { readFile, readdir, unlink, writeFile } from "fs/promises";
+import { join } from "path";
+
+console.time("Done");
+
+const repo = process.env.github_repository ?? "Gabe616/VendettaThemesPlus";
+
+const { list } = JSON.parse(
+  await readFile(join("../../", "iconpacks", "list.json"), "utf8")
+);
+
+for (const f of (
+  await readdir(join("../../../", "trees"), {
+    withFileTypes: true,
+  })
+).filter((x) => x.isFile() && x.name.endsWith(".txt")))
+  await unlink(join("../../../", "trees", f.name));
+
+for (const ic of list) {
+  console.log(`Parsing tree for '${ic.id}'`);
+
+  let user, path;
+  if (ic.load) {
+    const split = ic.load.split("/");
+    user = split.slice(3, 5).join("/");
+    path = split.slice(6);
+  } else {
+    user = repo;
+    path = ["assets", ic.id];
+  }
+
+  try {
+    const paths = (
+      await (
+        await fetch(
+          `https://api.github.com/repos/${user}/git/trees/master?recursive=1`
+        )
+      ).json()
+    ).tree
+      .filter((x) => x.path.startsWith(path.join("/")))
+      .map((x) => x.path.split("/").slice(path.length).join("/"))
+      .filter((x) => x.length > 0);
+
+    await writeFile(
+      join("../../../", "trees", `${ic.id}.txt`),
+      paths.join("\n")
+    );
+  } catch (e) {
+    console.log(`Failed to parse tree for '${ic.id}'!`);
+    continue;
+  }
+}
+
+console.timeEnd("Done");
