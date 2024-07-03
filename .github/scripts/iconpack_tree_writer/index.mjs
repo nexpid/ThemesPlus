0528064563cbfd4682658762bce0152fa7536b85
nexpid <60316309+nexpid@users.noreply.github.com>
2024-07-03 11:13:32 +0200

    feat(scripts): only add required files

--- .github/scripts/iconpack_tree_writer/index.mjs
+++ .github/scripts/iconpack_tree_writer/index.mjs
@@ -12,17 +12,24 @@ for (const ic of list) {
 
   const split = ic.load.split("/");
   const user = split.slice(3, 5).join("/");
+  const branch = split[5];
   const path = split.slice(6, -1);
 
   try {
     const paths = (
       await (
         await fetch(
-          `https://api.github.com/repos/${user}/git/trees/master?recursive=1`
+          `https://api.github.com/repos/${user}/git/trees/${branch}?recursive=1`
         )
       ).json()
     ).tree
-      .filter((x) => x.path.startsWith(path.join("/")))
+      .filter(
+        (x) =>
+          x.path.startsWith(path.join("/")) &&
+          ["png", "jpg", "webm", "lottie"].some((ext) =>
+            x.path.endsWith(`${ic.suffix}.${ext}`)
+          )
+      )
       .map((x) => x.path.split("/").slice(path.length).join("/"))
       .filter((x) => x.length > 0);
 
